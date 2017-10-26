cb867eaad4b73dfafc1a40f3705cc513ab51a3a0
Angel Coco Chen <angel@finaxar.com>
2017-10-26 22:28:53 +0800

    Remove extra files and content

--- react-fundamentals/contacts/src/App.js
+++ react-fundamentals/contacts/src/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from "react"
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts
-    return (
-      <ol>
-        {people.map(({ name }) => {
-          return <li key={name}>{name}</li>
-        })}
-      </ol>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList
-          contacts={[
-            { name: "Brienne" },
-            { name: "Tormund" },
-            { name: "Davos" }]}
-        />
       </div>
     )
   }
