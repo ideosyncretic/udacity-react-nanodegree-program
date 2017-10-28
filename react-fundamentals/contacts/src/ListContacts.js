7f3973e8b803b16f966189b5b2c00b2fef6a05ab
Angel Coco Chen <angel@finaxar.com>
2017-10-29 01:13:47 +0800

    Add query state and controlled component

--- react-fundamentals/contacts/src/ListContacts.js
+++ react-fundamentals/contacts/src/ListContacts.js
@@ -6,35 +6,53 @@ class ListContacts extends Component {
     contacts: PropTypes.array.isRequired,
     onRemoveContact: PropTypes.func.isRequired
   }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
   render () {
     const {
       contacts,
       onRemoveContact
     } = this.props
     return (
-      <ol className="contact-list">
-        {contacts.map(contact =>
-          <li
-            key={contact.id}
-            className="contact-list-item"
-          >
-            <div
-              className="contact-avatar"
-              style={{ backgroundImage: `url(${contact.avatarURL})`}}
-             />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button
-              className="contact-remove"
-              onClick={() => {onRemoveContact(contact)}}
-            >
-              Remove
-            </button>
-          </li>
-        )}
-      </ol>
+      <div className="list-contacts">
+        <input
+          type="text"
+          className="search-contacts"
+          placeholder="Search contacts"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+        />
+        <ol className="contact-list">
+          {contacts.map(contact =>
+            <li
+              key={contact.id}
+              className="contact-list-item"
+              >
+                <div
+                  className="contact-avatar"
+                  style={{ backgroundImage: `url(${contact.avatarURL})`}}
+                />
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button
+                  className="contact-remove"
+                  onClick={() => {onRemoveContact(contact)}}
+                  >
+                    Remove
+                  </button>
+                </li>
+              )}
+            </ol>
+      </div>
     )
  }
 }
