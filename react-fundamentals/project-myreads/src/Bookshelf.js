dd11af9e75824028dd4acde0c74edef99eca0d37
Angel Coco Chen <angel@finaxar.com>
2017-11-12 23:22:01 +0800

    Remove hardcoded books and add state to Bookshelf

--- react-fundamentals/project-myreads/src/Bookshelf.js
+++ react-fundamentals/project-myreads/src/Bookshelf.js
@@ -2,34 +2,21 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class Bookshelf extends Component {
+  state = {
+    books: []
+  }
   render () {
+    const { books } = this.state
     const { title } = this.props
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
+            { books.length
+              ? books.map(book => <li><Book /></li>)
+              : "No books in this shelf yet."
+            }
           </ol>
         </div>
       </div>
