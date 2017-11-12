418afe1326fc4fab5c9f2284b0edf25ff5632c4a
Angel Coco Chen <angel@finaxar.com>
2017-11-13 00:40:00 +0800

    Make Book shelf selection work
    - update Book select input to controlled component

--- react-fundamentals/project-myreads/src/Book.js
+++ react-fundamentals/project-myreads/src/Book.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Book = ({ book, handleUpdate }) => {
-  const {
-    id,
-    title,
-    authors,
-    imageLinks
-  } = book
+class Book extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: this.props.book.shelf
+    }
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
-        <div className="book-shelf-changer">
-          <select onChange={(event) => handleUpdate(book, event.target.value)}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const value = event.target.value
+    const book = this.props.book
+    this.setState({value})
+    this.props.handleUpdate(book, value)
+  }
+
+  render () {
+    const { value } = this.state
+    const { book } = this.props
+    const {
+      title,
+      authors,
+      imageLinks
+    } = book
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select value={value} onChange={this.handleChange}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors.map(author => author)}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{authors.map(author => author)}</div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Book
