9553a954707da184538601544adc7c990d30cb3f
Angel Coco Chen <angel@finaxar.com>
2017-11-12 22:48:46 +0800

    Add routes for home page and search page

--- react-fundamentals/project-myreads/src/App.js
+++ react-fundamentals/project-myreads/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ListBooks from './ListBooks'
+import Search from './Search'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+class BooksApp extends Component {
 
   render() {
     return (
-      <div className="app">
-        <ListBooks />
+      <div>
+        <Route exact path="/" component={ListBooks} />
+        <Route exact path="/search" component={Search} />
       </div>
     )
   }
 }
+
 
 export default BooksApp
--- react-fundamentals/project-myreads/src/ListBooks.js
+++ react-fundamentals/project-myreads/src/ListBooks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ListBooks = () => {
   return (
@@ -158,7 +159,7 @@ const ListBooks = () => {
         </div>
       </div>
       <div className="open-search">
-        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        <Link to='/search'>Add a book</Link>
       </div>
     </div>
   )
